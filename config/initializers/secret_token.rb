fa5597b3bb80e23956684b635f7bfcc0bd9acb5c
Olivier Gosse-Gardet <ext-ogossegardet@leaseplan.fr>
2014-08-04 07:48:15 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appcasclient::Application.config.secret_token = '58fa591c1a16497855fae4d923e8bbffbdb14674ee775292b1d659f8e1b132acff76527a32d44b84aaeed26b7b0fa7e08cb2dd45067b150cb138472b0b94d95e'
